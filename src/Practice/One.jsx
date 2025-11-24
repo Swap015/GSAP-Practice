1423d64b296a0f97dd5ca40968b4901bc2a4fd51
Swapnil Motghare <swapnilmotghare44@gmail.com>
2025-11-24 14:28:55 +0530

    Added Example One

---
+++ src/Practice/One.jsx
@@ -0,0 +1,30 @@
+import gsap from 'gsap';
+import React, { useEffect, useRef } from 'react'
+
+const One = () => {
+    const boxRef = useRef(null);
+
+    useEffect(() => {
+        gsap.to(boxRef.current, {
+            x: 700,
+            backgroundColor: "red",
+            rotate: 720,
+            duration: 2,
+            delay: 1
+        })
+    }, [])
+
+    return (
+        <div className='min-h-screen bg-gray-500 border-black p-20'>
+            <div ref={boxRef} className="w-40 h-40 bg-yellow-500"></div>
+        </div>
+    )
+}
+
+export default One;
+
+
+
+
+
+
